86d0d4bd19957dbcbb019589b7b0233d34bda644
Tanisha <tanwartanisha53@gmail.com>
2025-11-02 17:45:49 +0530

    Initial React App Project

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Footer/>
+        </div>
+    );
+}
+
+
+export default App;
